9718753ef81960365c932841bdd71cbbd7cc1ee3
jovanovicmilica <mikipn99@gmail.com>
2019-06-22 10:41:00 +0200

    proba

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $(".dropdown-trigger").dropdown()
+});
+
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+
+    
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+
+  $(document).ready(function(){
+    $('.modal').modal();
+  });
+        
